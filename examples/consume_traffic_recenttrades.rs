1ba1c5fd088ec10a09cb81ec0b3ad06b1a60a787
GroovieGermanikus <groovie@mango.markets>
2024-10-02 09:33:34 +0200

    dojo ws working

--- examples/consume_traffic_recenttrades.rs
+++ examples/consume_traffic_recenttrades.rs
@@ -1,8 +1,9 @@
 use env_logger::Env;
-use serde_json::json;
+use serde_json::{json, Value};
 use std::env;
 use std::thread::Thread;
 use std::time::Duration;
+use anyhow::bail;
 
 use tokio::sync::mpsc::{Sender, UnboundedSender};
 use tokio::time::sleep;
@@ -12,19 +13,28 @@ use websocket_tungstenite_retry::websocket_stable::StableWebSocket;
 #[tokio::main]
 async fn main() {
     env_logger::Builder::from_env(
-        Env::default().default_filter_or("websocket_tungstenite_retry::websocket_stable=debug"),
+        Env::default().default_filter_or("websocket_tungstenite_retry::websocket_stable=info"),
     )
     .init();
 
     let ws_url = "wss://api.mngo.cloud/dojo/v1/".to_string();
 
     let mut ws = loop {
-        let attempt = StableWebSocket::new_with_timeout(
+        let attempt = StableWebSocket::new_with_timeout_and_success_callback(
             Url::parse(ws_url.as_str()).unwrap(),
-            json!({
-            "command": "subscribe",
-            "mintIds": ["J1toso1uCk3RLmjorhTtrVwY9HJ7X8V9yYac6Y7kGCPn-So11111111111111111111111111111111111111112"],
-        }), Duration::from_secs(5)
+                json!({
+                "command": "subscribe",
+                "mintIds": ["J1toso1uCk3RLmjorhTtrVwY9HJ7X8V9yYac6Y7kGCPn-So11111111111111111111111111111111111111112"],
+            }),
+            |msg| {
+                // {"Status":{"success":true,"message":"subscribed to J1toso1uCk3RLmjorhTtrVwY9HJ7X8V9yYac6Y7kGCPn-So11111111111111111111111111111111111111112 market with id: J1toso1uCk3RLmjorhTtrVwY9HJ7X8V9yYac6Y7kGCPn-So11111111111111111111111111111111111111112"}}
+                let maybe_value = serde_json::from_str::<Value>(msg)?;
+                match maybe_value["Status"]["success"].as_bool() {
+                    Some(_) => Ok(()),
+                    None => bail!("unexpected message: {}", msg)
+                }
+            },
+            Duration::from_secs(5),
         ).await;
 
         match &attempt {
